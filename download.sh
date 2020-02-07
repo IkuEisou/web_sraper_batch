cad7a2fec7963b77c08bb57387bad93935fb3016
yuyc <cleverforever@gmail.com>
2020-02-07 17:07:04 +0900

    output as csv

--- download.sh
+++ download.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 . .env
 param_file=$1
 
 for line in $(cat $param_file); do
     curl --location --request GET 'https://api.webscraper.io/api/v1/scraping-job/'${line}'/json?api_token='${WS_TOCKEN} >>result.json
 done
-cat result.json | jq '.title' >>result.csv
+sed -e "s/web-scraper-start-url/key/g" result.json >result_r.json
+jq '.key |= (sub("https://www.bloomberg.com/quote/";"")) | [.key, .title] | @csv' result_r.json >result.csv
+# sed -e "s/\"//g" result.csv >result_r.csv
